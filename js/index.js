795095912c621ac93c9b38a713b6f40b279db0a4
erickgamez15 <erickgamezsanchez@gmail.com>
2022-04-15 00:49:17 -0500

    Página terminada

--- js/index.js
+++ js/index.js
@@ -13,28 +13,50 @@ window.addEventListener('scroll', function(){
     header.classList.toggle('scrolling-active', windowPosition)
 })
 
-//Animaciones index
-window.sr = ScrollReveal();
-    sr.reveal('.nav', {
-        duration: 1000,
-        origin: 'bottom',
-        distance: '-100px'
-    });
-
-//Animaciones about
-const cartas = document.querySelector('.cartas');
-console.log(cartas.children[0])
+//Animación nav
 window.sr = ScrollReveal()
-sr.reveal('.cartas', {
-    duration: 3000,
-    origin: 'top',
+sr.reveal('.nav',{
+    duration: 750,
+    origin: 'bottom',
+    distance: '-50px'
+});
+
+//Animación nav-pages
+window.sr = ScrollReveal()
+sr.reveal('.nav-pages',{
+    duration: 750,
+    origin: 'bottom',
+    distance: '-50px'
+});
+
+//Animaciones index
+window.sr = ScrollReveal()
+sr.reveal('.image', {
+    duration: 2000,
+    origin: 'left',
     distance: '-100px'
 });
+
+window.sr = ScrollReveal()
+sr.reveal('.secondText', {
+    duration: 2000,
+    origin: 'rigth',
+    distance: '-100px'
+});  
     
-//Animaciones en JS servicios / services
-window.sr = ScrollReveal();
-sr.reveal('.container-services', {
-    duration: 3000,
+window.sr = ScrollReveal()
+sr.reveal('.thirdText', {
+    duration: 2000,
+    origin: 'rigth',
+    distance: '-100px'
+});
+
+//Animaciones about
+const cartas = document.querySelector('.cards')
+console.log(cartas.children[0])
+window.sr = ScrollReveal()
+sr.reveal('.cards', {
+    duration: 2000,
     origin: 'top',
     distance: '-100px'
 });
